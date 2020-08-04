e1f8812ff732c54cf8903b91ee5ba80f55aaaa03
Federico J. Leiva <federicoleivap@gmail.com>
2020-08-03 21:11:58 -0300

    Agregados mas Tests Unitarios.

--- tests/algoritmo.test.js
+++ tests/algoritmo.test.js
@@ -23,4 +23,23 @@ test('La función "rotateMatrix" debería rotar una matriz', () => {
         ['v', ' ', 'p', ' ', ' '],
         ['o', 's', 'u', 'A', ' ']
     ])
+});
+
+test('La función "matrixToStr" debería convertir la matríz a un string', () => {
+    expect(algoritmos.matrixToStr([
+        ['D', 'e', 'v', 'o'],
+        ['f', 'f', ' ', 's'],
+        ['e', ' ', 'p', 'u'],
+        ['s', 'o', ' ', 'A'],
+        ['T', 'R', ' ', ' ']
+    ])).toBe("Devoff se puso ATR  ");
+});
+
+test('La función "genRotatedMatrix" debería generar una matriz con el tamaño correcto mediante un string encriptado', () => {
+    expect(algoritmos.genRotatedMatrix('DfesTef oRv p  osuA', 4)).toStrictEqual([
+        ['D', 'f', 'e', 's', 'T'],
+        ['e', 'f', ' ', 'o', 'R'],
+        ['v', ' ', 'p', ' ', ' '],
+        ['o', 's', 'u', 'A', ' ']
+    ])
 })
